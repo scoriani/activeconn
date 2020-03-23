5ac7afee1d5c311c76e34b6fa0d07a45dd63ef82
Silvano Coriani <scoriani@microsoft.com>
2020-03-23 12:34:46 +0100

    initial commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace SQLConnectionPooling
+{
+    class Program
+    {
+        static PerformanceCounter pooledConn;
+        static Process currentProcess;
+        static int numActiveConns;
+
+        static void Main(string[] args)
+        {
+            currentProcess = Process.GetCurrentProcess();
+            pooledConn = new PerformanceCounter(categoryName: ".NET Data Provider for SqlServer", counterName: "NumberOfActiveConnections", instanceName: currentProcess.ProcessName.ToLower() + "[" + currentProcess.Id + "]");
+
+            using (SqlConnection cnn = new SqlConnection("Server=tcp:localhost,1433;Initial Catalog=test;Integrated Security=SSPI;Connection Lifetime=0"))
+            {
+                cnn.StateChange += Cnn_StateChange;
+                cnn.Open();
+
+                numActiveConns = (int)pooledConn.NextValue();
+                Console.WriteLine("\n[{0}] -- Number of Active Connections: {1} -- \n", DateTime.Now.ToUniversalTime(),numActiveConns);
+            }
+
+            numActiveConns = (int)pooledConn.NextValue();
+            Console.WriteLine("\n[{0}] -- Number of Active Connections: {1} -- \n", DateTime.Now.ToUniversalTime(),numActiveConns);
+
+            Console.ReadLine();
+        }
+
+        private static void Cnn_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            Console.WriteLine("[{0}] -- CurrentState: {0} -- OriginalState: {1} \n", DateTime.Now.ToUniversalTime(), e.CurrentState, e.OriginalState);
+        }
+    }
+}
